ea885a2c7fc62d9b741b950a6307880dd45e3374
Andrew Cassetti <amcassetti@gmail.com>
2020-04-20 09:26:05 -0400

    rebuild ui

---
+++ ui/build/precache-manifest.526c874b9476c94cec95e280900fc4a2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e34a4bba4f302076ca1a002ee661e903",
+    "url": "/index.html"
+  },
+  {
+    "revision": "735f43daf181f1746800",
+    "url": "/static/css/main.7f1fbd45.chunk.css"
+  },
+  {
+    "revision": "21309b114ecb6aae75e9",
+    "url": "/static/js/2.13273681.chunk.js"
+  },
+  {
+    "revision": "735f43daf181f1746800",
+    "url": "/static/js/main.2e27e1e9.chunk.js"
+  },
+  {
+    "revision": "bd683d45e2f0afafa729",
+    "url": "/static/js/runtime~main.9c23384b.js"
+  },
+  {
+    "revision": "3ae794fd864ec3ffbefc5d488900ef7c",
+    "url": "/static/media/bg.3ae794fd.svg"
+  },
+  {
+    "revision": "e157bcf70d04cd68d709b2622d53bbd7",
+    "url": "/static/media/btc-logo.e157bcf7.svg"
+  },
+  {
+    "revision": "42d0a1b9cb789da4e757e29944002d7c",
+    "url": "/static/media/dai-logo.42d0a1b9.png"
+  },
+  {
+    "revision": "f8c4503d175d147034c6b62c55b91c5e",
+    "url": "/static/media/dai-logo.f8c4503d.svg"
+  },
+  {
+    "revision": "f943f4b90e2f6ce7b26e1d5dab90693d",
+    "url": "/static/media/exchange.f943f4b9.svg"
+  },
+  {
+    "revision": "88be4f60ee7adf4a8298c8331ab4cc0f",
+    "url": "/static/media/github.88be4f60.svg"
+  },
+  {
+    "revision": "ec47866216e80bae9339d5a5919d2f0b",
+    "url": "/static/media/reddit.ec478662.svg"
+  },
+  {
+    "revision": "9f244c8d826fa2e6f1bc97656cf3135c",
+    "url": "/static/media/renvm-logo.9f244c8d.svg"
+  },
+  {
+    "revision": "7ad107a3d467d72053172fb8cd291372",
+    "url": "/static/media/scrypt.7ad107a3.node"
+  },
+  {
+    "revision": "c925df35d6d3fa95ca99b4662c3ec600",
+    "url": "/static/media/stream.c925df35.svg"
+  },
+  {
+    "revision": "da2b913b86006f6c3862231b9fa723f9",
+    "url": "/static/media/telegram.da2b913b.svg"
+  },
+  {
+    "revision": "b6e415cfb67621d32e7a5c3f2cd4260b",
+    "url": "/static/media/transfer.b6e415cf.svg"
+  },
+  {
+    "revision": "969888a38dce7cec62dbdb9a3fbea3f9",
+    "url": "/static/media/twitter.969888a3.svg"
+  }
+]);
